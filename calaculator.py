d76be18970f7b00a5f3d0213900f587c3aa699aa
eliya.sk <92324345+eliyaaa1pa@users.noreply.github.com>
2023-06-26 23:53:29 +0330

    Add files via upload

---
+++ calaculator.py
@@ -0,0 +1,13 @@
+a = input("alamat: ")
+
+adad1 = int(input("adad1: "))
+adad2 = int(input("adad2: "))
+
+if a == "+" :
+    print(adad1 + adad2)
+if  a == "-":
+    print(adad1 - adad2)
+if a == "*":
+    print(adad1 * adad2)
+if a =="/":
+    print(adad1 / adad2)
